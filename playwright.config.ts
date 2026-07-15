47a8431f6d39725c10ead4ad5c12c21263c174d5
Alexandre Agostini <a.agostini.fr@gmail.com>
2026-07-15 16:43:26 +0200

    fix: build shared package for e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
   testDir: "tests/e2e",
   webServer: {
-    command: "npm run dev -w @cr-league/web -- --host 127.0.0.1 --port 4978",
+    command: "npm run build -w @cr-league/shared -- --force && npm run dev -w @cr-league/web -- --host 127.0.0.1 --port 4978",
     url: "http://127.0.0.1:4978",
     reuseExistingServer: !process.env.CI
   },
